0d41024fd0a7359b2d86b6edc1f709b73f4d1980
kpuja <kpuja@gmail.com>
2019-11-09 13:52:42 -0500

    09 nov commited

--- CustomerUpdateAPI/src/main/java/com/customerApi/update/controller/CustomerUpdateController.java
+++ CustomerUpdateAPI/src/main/java/com/customerApi/update/controller/CustomerUpdateController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.customerApi.update.UpdateCustomerService;
 import com.customerApi.update.modal.Customer;
+import com.customerApi.update.service.UpdateCustomerService;
 
 
 @RestController
--- CustomerUpdateAPI/src/main/java/com/customerApi/update/service/UpdateCustomerService.java
+++ CustomerUpdateAPI/src/main/java/com/customerApi/update/service/UpdateCustomerService.java
@@ -1,4 +1,4 @@
-package com.customerApi.update;
+package com.customerApi.update.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
